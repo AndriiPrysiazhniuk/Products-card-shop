367a73d270244a12f4cadb0d3abcfb511a03eb0b
AndriiPrysizhniuk <sarutora18@gmail.com>
2023-02-05 22:36:28 +0200

    added ProductCard

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -8,7 +8,10 @@ export const ProductCard = ({title, description, unitPrice, img}: PropsType) => 
 
     return (
         <div>
-
+<h1>{title}</h1>
+        <p>{description}</p>
+            <img src={img} alt="image of notebooks"/>
+            {unitPrice}
         </div>
 
 
